025e743b41405ec6830d7f775d2824400447cce5
Mak Krnic <mkrnic@gmail.com>
2012-09-30 12:32:53 +0200

    Initial commit - added the first version and gitignore.

---
+++ ferwebProsirenje.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          ferwebProsirenje
+// @namespace     http://www.fer.unizg.hr/
+// @description   Izmjena za ferweb tako da prosiri stranicu s kalendarom na punu sirinu browsera kako bi kalendar bio pregledniji.
+//
+// @include *www.fer.hr/kalendar
+// @include *www.fer.unizg.hr/kalendar
+// ==/UserScript==
+
+
+//alert ('Using ferwebProsirenje.');
+
+var frame = document.getElementById ('window_div');
+frame.style.width = '100%';
+frame.style.maxWidth = '100%';
